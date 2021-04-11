b3c5847d50e03df294a4f1ed6e0e8bdb23fc1b7c
Pawan <bpawan96@gmail.com>
2021-04-11 15:38:35 +0530

    modified scrapping reference scrap.rake.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # SCRAPING
 
 * Reference: "https://towardsdatascience.com/job-board-scraping-with-rails-872c432ed2c8"
+* Reference: "https://nokogiri.org/tutorials/installing_nokogiri.html"
 
-* 
+* nokogiri.rb file for practice.
 * ...
--- nokogiri.rb
+++ nokogiri.rb
@@ -2,19 +2,33 @@ require 'nokogiri'
 require 'open-uri'
 
 # Fetch and parse HTML document
-doc = Nokogiri::HTML(URI.open('https://nokogiri.org/tutorials/installing_nokogiri.html'))
+# doc = Nokogiri::HTML(URI.open('https://nokogiri.org/tutorials/installing_nokogiri.html'))
+doc = Nokogiri::HTML(URI.open('https://www.celebritynetworth.com/list/top-100-richest-people-in-the-world/'))
 
 # Search for nodes by css
-doc.css('nav ul.menu li a', 'article h2').each do |link|
-  puts link.content
-end
+# doc.css('nav ul.menu li a', 'article h2').each do |link|  # For 'https://nokogiri.org/tutorials/installing_nokogiri.html'
+# doc.css('div.number','div.summary h2', 'div.net_worth').each do |link|
+#   puts link.content
+# end
 
 # Search for nodes by xpath
-doc.xpath('//nav//ul//li/a', '//article//h2').each do |link|
-  puts link.content
-end
+# doc.xpath('//nav//ul//li/a', '//article//h2').each do |link|
+#   puts link.content
+# end
 
 # Or mix and match
 # doc.search('nav ul.menu li a', '//article//h2').each do |link|
 #   puts link.content
 # end
+
+# Search for nodes by .search()
+billionaires = doc.search('a.anchor.clearfix')
+billionaires.each do |bill|
+  # puts bill
+  name = bill.search('h2.title').text.gsub('Net Worth', '')
+  net_worth = bill.search('div.net_worth').text
+  bio = bill.search('div.bio').text
+  rank = bill.search('div.number').text
+  puts "Name: #{name}, Rank: #{rank}, Net Worth: #{net_worth}"
+  puts bio
+end
